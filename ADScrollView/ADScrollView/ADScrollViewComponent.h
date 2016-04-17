2af39caa27d08e24e43184568a2ca7e0c62a8a83
Mark <18929562811@163.com>
2016-04-17 11:09:13 +0800

    no message

---
+++ ADScrollView/ADScrollView/ADScrollViewComponent.h
@@ -0,0 +1,13 @@
+//
+//  ADScrollViewComponent.h
+//  ADScrollView
+//
+//  Created by 叶杨杨 on 16/4/17.
+//  Copyright © 2016年 叶杨杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADScrollViewComponent : UIView
+
+@end
---
+++ ADScrollView/ADScrollView/ADScrollViewComponent.m
@@ -0,0 +1,21 @@
+//
+//  ADScrollViewComponent.m
+//  ADScrollView
+//
+//  Created by 叶杨杨 on 16/4/17.
+//  Copyright © 2016年 叶杨杨. All rights reserved.
+//
+
+#import "ADScrollViewComponent.h"
+
+@implementation ADScrollViewComponent
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
